b298877eba2fa670c43833151802ea89a2b1158b
zoe <zoendev@gmail.com>
2022-09-02 15:50:39 +0900

    useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
 import Button from "./Button";
 import styles from "./App.module.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => setCounter((prev) => prev + 1);
+  console.log("Run All the Time");
+  // const iRunOnlyOnce = () => {
+  //   console.log("Run Only Once.");
+  // };
+  // useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log("Call the API");
+  }, []);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text={"start"} />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click</button>
     </div>
   );
 }
